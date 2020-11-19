b5a1adf4233a343432df93efa8d15a6472760a0b
Rachel Kim <rachelkim8084@gmail.com>
2020-11-19 00:24:13 -0800

    Set up basic layout of code.

--- src/test/java/HandleTruthTest.java
+++ src/test/java/HandleTruthTest.java
@@ -1,8 +1,19 @@
 import org.junit.Test;
 
+import java.util.Set;
+import java.util.TreeMap;
+import static org.junit.Assert.assertEquals;
+
 public class HandleTruthTest {
     @Test
     public void makeshiftTest() {
         HandleTruth.wordCount();
+    }
+
+    @Test
+    public void realTest() {
+        TreeMap<Integer, Set<String>> actual = HandleTruth.wordCount();
+        TreeMap<Integer, Set<String>> expected = null;
+        assertEquals(expected, actual);
     }
 }
